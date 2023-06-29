8e223b7cd5a531e02bc761c0aaf0e0b22b40c9fb
Titus Wormer <tituswormer@gmail.com>
2023-06-29 12:45:19 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -2,53 +2,52 @@ import assert from 'node:assert/strict'
 import test from 'node:test'
 import {parse} from 'acorn'
 import {positionFromEstree} from './index.js'
-import * as mod from './index.js'
 
-test('positionFromEstree', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['positionFromEstree'],
-    'should expose the public api'
-  )
+test('positionFromEstree', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'positionFromEstree'
+    ])
+  })
 
-  assert.deepEqual(
-    positionFromEstree(),
-    undefined,
-    'should support a missing node'
-  )
+  await t.test('should support a missing node', async function () {
+    assert.deepEqual(positionFromEstree(), undefined)
+  })
 
-  assert.deepEqual(
-    positionFromEstree(parse('x', {ecmaVersion: 2020})),
-    undefined,
-    'should support node w/o `loc`s'
-  )
+  await t.test('should support node w/o `loc`s', async function () {
+    assert.deepEqual(
+      positionFromEstree(parse('x', {ecmaVersion: 2020})),
+      undefined
+    )
+  })
 
-  assert.deepEqual(
-    positionFromEstree(parse('x', {ecmaVersion: 2020, locations: true})),
-    {
-      start: {line: 1, column: 1, offset: 0},
-      end: {line: 1, column: 2, offset: 1}
-    },
-    'should support node w/ `loc`s'
-  )
+  await t.test('should support node w/ `loc`s', async function () {
+    assert.deepEqual(
+      positionFromEstree(parse('x', {ecmaVersion: 2020, locations: true})),
+      {
+        start: {line: 1, column: 1, offset: 0},
+        end: {line: 1, column: 2, offset: 1}
+      }
+    )
+  })
 
-  assert.deepEqual(
-    positionFromEstree(parse('x', {ecmaVersion: 2020, ranges: true})),
-    undefined,
-    'should support node w/ `range`s'
-  )
+  await t.test('should support node w/ `range`s', async function () {
+    assert.deepEqual(
+      positionFromEstree(parse('x', {ecmaVersion: 2020, ranges: true})),
+      undefined
+    )
+  })
 
-  assert.deepEqual(
-    positionFromEstree({loc: {start: {}, end: {}}}),
-    undefined,
-    'should handle points w/o line/column'
-  )
+  await t.test('should handle points w/o line/column', async function () {
+    assert.deepEqual(positionFromEstree({loc: {start: {}, end: {}}}), undefined)
+  })
 
-  assert.deepEqual(
-    positionFromEstree({
-      loc: {start: {line: -1, column: -1}, end: {line: 1, column: 0}}
-    }),
-    undefined,
-    'should handle points w/ too low values'
-  )
+  await t.test('should handle points w/ too low values', async function () {
+    assert.deepEqual(
+      positionFromEstree({
+        loc: {start: {line: -1, column: -1}, end: {line: 1, column: 0}}
+      }),
+      undefined
+    )
+  })
 })
